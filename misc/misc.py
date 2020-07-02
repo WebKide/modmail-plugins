6fb0589d6b5b186d9a3660b7119d509a2db0d927
kuro <46526001+WebKide@users.noreply.github.com>
2020-07-02 23:22:48 +0100

    Update misc.py

--- misc/misc.py
+++ misc/misc.py
@@ -1,4 +1,10 @@
-import discord, unicodedata, inspect, urllib.request
+import discord
+import unicodedata2 as unicodedata
+try:
+    import inspect
+except:
+    pass
+import urllib.request
 
 from discord.ext import commands
 
