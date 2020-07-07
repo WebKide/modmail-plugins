02c3c7b892bb16dc354dcc92a4cd85c1683b4cae
kuro <46526001+WebKide@users.noreply.github.com>
2020-07-07 15:24:15 +0100

    Create on-message.py

---
+++ on-message/on-message.py
@@ -0,0 +1,47 @@
+"""
+MIT License
+Copyright (c) 2020 WebKide [d.id @323578534763298816]
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import discord
+from random import choice as c
+
+class OnMessage(commands.Cog):
+    """(∩｀-´)⊃━☆ﾟ.*･｡ﾟ non-commands, bot responds to text in message """
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    # +------------------------------------------------------------+
+    # |               RESPONDS TO TEXT IN MESSAGE                  |
+    # +------------------------------------------------------------+
+hello = random.choice(['Hello ', 'Hi there ', 'Howdy '])
+msg = message.content[4:]
+
+async def on_message(message):
+    if message.content.startswith('I am'):
+        await client.send_message(message.channel, f'{hello}' + f'*{msg}*' + ', I am **Moderator** 🅑🅞🅣')
+    else:
+        pass
+
+    # +------------------------------------------------------------+
+    # |                                                            |
+    # +------------------------------------------------------------+
+    
+def setup(bot):
+    bot.add_cog(OnMessage(bot))
