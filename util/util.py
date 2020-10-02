00654c84552c8581fb56b065041fddac3e9bfbb0
kuro <46526001+WebKide@users.noreply.github.com>
2020-10-02 18:41:01 +0100

    Update util.py

--- util/util.py
+++ util/util.py
@@ -36,17 +36,42 @@ class Util(commands.Cog):
     @commands.command(description="Bot's enviroment stats", no_pm=True)
     async def util(self, ctx):
         """ Show Bot's env stats """
-        try:
-            memory_usage = self.bot.process.memory_full_info().uss / 1024**2
-            cpu_usage = self.bot.process.cpu_percent() / psutil.cpu_count()
-            
-            cpu = f'CPU Usage:  {cpu_usage:.2f}% CPU processing'
-            ram = f'RAM Usage:  {memory_usage:.2f} MiB of memory'
-            
-            result = f'{cpu}\n{ram}'
-            await ctx.send(result)
-        except Exception as e:
-            await ctx.send(e)
+        pp = psutil.Process()
+        x = psutil.virtual_memory()
+        
+        cpu_u = pp.cpu_percent() / psutil.cpu_count()
+		m_us = pp.memory_full_info().uss / 1024**2
+		m_pe = pp.memory_percent()
+        
+		mt = x.total / 1024**2
+		mf = x.free / 1024**2
+		ma = x.active / 1024**2
+		mu = x.used / 1024**2
+		mv = x.available / 1024**2
+		mc = x.cached / 1024**2
+        
+		c = f'CPU Usage:  {cpu_u:.2f}% CPU processing'
+		r = f'RAM Usage:  {m_us:.2f} MiB of memory'
+		p = f'RAM Percentage:  {m_pe:.2f}% MiB'
+		q = f'Total Memory:  {mt:.2f} MiB'
+		w = f'Free Memory:  {mf:.2f} MiB'
+		e = f'Active Memory:  {ma:.2f} MiB'
+		t = f'Used Memory:  {mu:.2f} MiB'
+		y = f'Available Memory:  {mv:.2f} MiB'
+		u = f'Cached Memory:  {mc:.2f} MiB'
+        
+		result = f'@_Geek(stats, bot):\n\n{c}\n{r}\n{p}\n\n{q}\n{w}\n{e}\n{t}\n{y}\n{u}'
+        
+		THRESHOLD = 100 * 1024 * 1024  # 100MB
+
+		try:
+		    await ctx.send(f'```py\n{result}```')
+		    if x.available <= THRESHOLD:
+		        await ctx.send('**`SYSTEM WARNING!`**')
+		    else:
+		        pass # silently
+		except Exception as e:
+		    await ctx.send(f'```py\n{e}```')
 
 
 def setup(bot):
