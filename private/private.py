ffe4db7063000011d9eb77462067736f8ddc83a6
kuro <46526001+WebKide@users.noreply.github.com>
2020-10-07 19:45:38 +0100

    many bugs fixed

--- private/private.py
+++ private/private.py
@@ -26,7 +26,7 @@ from discord.ext import commands
 
 
 class Private(commands.Cog):
-    """─=≡Σ(つಠ益ಠ)つ this is a private cog for my personal discord guild, so it won't work for yours! """
+    """─=≡Σ(つಠ益ಠ)つ private cog for my personal discord guild, it won't work for yours! """
     def __init__(self, bot):
         self.bot = bot
         self.mods = [
@@ -39,9 +39,8 @@ class Private(commands.Cog):
     # +------------------------------------------------------------+
     # |                 PUSH-NOTIFICATION                          |
     # +------------------------------------------------------------+
-    @commands.command(description='Personal Guild command', aliases=['poke', 'push', 'notification', 'everyone'], no_pm=True)
-    @commands.has_any_role('Admin', 'Mod', 'DJ', 'Owner')
-    async def nudge(self, ctx, *, unique_event: str = None):
+    @commands.command(description='Personal Guild cmd', aliases=['poke', 'push', 'notification'], no_pm=True)  # @commands.has_any_role('Admin', 'Mod', 'DJ', 'Owner')
+    async def nudge(self, ctx, *, _event_today: str = None):
         """
         ─=≡Σ(つಠ益ಠ)つ command to send a Push-notification in text-channel
         
@@ -54,30 +53,29 @@ class Private(commands.Cog):
         except discord.Forbidden:
             pass
 
-        # if ctx.author.id not in (mod[1] for mod in self.mods):
-        #    return
+        if ctx.author.id not in (mod[1] for mod in self.mods):
+            return
 
-        u = ctx.message.author.mention
         channel =  ctx.channel or ctx.message.channel
-        error_msg = f"{u}, update this channel's **Topic** for this `command` to work!"
-        help_msg = "Tip: check other channel's Topics to get an idea of how to format their content."
+        e_m = f"{ctx.message.author.mention}, update this channel's **Topic** for this `command` to work!"
+        h_m = "\n\n**Tip:** check other channel's Topics to get an idea of how to format their content."
 
-        try:
-            if isinstance(channel, discord.TextChannel):
+        if isinstance(channel, discord.TextChannel):
+            while True:
+                if not channel.topic:
+                    return await ctx.send(e_m + h_m, delete_after=23)
+
                 if '—' in channel.topic:
                     c_topic = channel.topic.replace('\n', '— ').split('—')[-1:]
-                    #  x = t.now(z('Asia/Calcutta')).strftime(f'%A %B %d')
-                    i =  '\U0001f538'
-                    g = ctx.message.guild
                     day_fix = f"{str(t.now(z('Asia/Calcutta')).strftime(f'%A %B %d'))} (IST)"
                     date_fixed = day_fix.replace('1 ', '1ˢᵗ ').replace('2 ', '2ⁿᵈ ')\
                                         .replace('3 ', '3ʳᵈ ').replace('4 ', '4ᵗʰ ')\
                                         .replace('5 ', '5ᵗʰ ').replace('6 ', '6ᵗʰ ')\
                                         .replace('7 ', '7ᵗʰ ').replace('8 ', '8ᵗʰ ')\
                                         .replace('9 ', '9ᵗʰ ').replace('0 ', '0ᵗʰ ')
-                    if unique_event is not None:
+                    if _event_today is not None:
                         h = random.choice(['for the celebration of', 'to observe', 'to honour'])
-                        v = f" and to gather together {h} **{unique_event}**."
+                        v = f" and to gather together {h} **{_event_today}**."
                     else:
                         v = random.choice([
                             " and to continue with yesterday's topic of discussion.",
@@ -85,18 +83,17 @@ class Private(commands.Cog):
                             " where we'll continue from where we left off yesterday.",
                             " and take part in the continuation of yesterday's discussion."
                         ])
-                    m = f"{i} {date_fixed} is a perfect day to listen to the "\
-                        f"**{g}** podcast{v}\n{''.join(c_topic)}"
+                    m = f"\U0001f538 {date_fixed} is a perfect day to listen to the "\
+                        f"**{ctx.message.guild}** podcast{v}\n\n{''.join(c_topic)}"
                     _nudge = await ctx.send(m)
                     try:
                         await _nudge.add_reaction('thankful:695101751707303998')
                     except discord.HTTPException:
-                        pass
-            else:
-                await ctx.send(error_msg + help_msg, delete_after=23)
-        except discord.ext.commands.errors.CommandInvokeError:  # AttributeError:
-            await ctx.send(error_msg + help_msg, delete_after=69)
+                        continue
 
+                else:
+                    continue
+            
 
 def setup(bot):
     bot.add_cog(Private(bot))
