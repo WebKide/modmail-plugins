c9da5d769be86ba3f866a980ec9dc3b19093823d
kuro <46526001+WebKide@users.noreply.github.com>
2023-03-01 05:13:23 +0000

    2023 UPDATE!!
    breaking changes, it should suffice to say that this command, and entire PLUGIN only works in my personal Guild, sorry for any inconvenience.

--- private/private.py
+++ private/private.py
@@ -1,6 +1,6 @@
 """
 MIT License
-Copyright (c) 2020 WebKide [d.id @323578534763298816]
+Copyright (c) 2020-2023 WebKide [d.id @323578534763298816]
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
@@ -18,156 +18,156 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-import discord, requests, traceback, aiohttp, random, asyncio
+import discord, asyncio, re
 
 from datetime import datetime as t
 from pytz import timezone as z
-from bs4 import BeautifulSoup, SoupStrainer
+from random import choice
 from discord.ext import commands
 
+_TZ = {
+    "IST": "Asia/Kolkata",
+    "BST": "Europe/London",
+    "EST": "America/New_York",
+    "PST": "America/Los_Angeles",
+    "BOT": "America/La_Paz"
+}
+
+p_c = f' the **{ctx.message.guild}** Podcast, '
+
+INTRO = [
+    f'*Turn off and tune into*{p_c}',
+    f'Bring auspiciousness to your day with{p_c}',
+    f'Hello and welcome to{p_c}',
+    f'It is a beautiful day to listen to{p_c}',
+    f'It is a nice day to listen to{p_c}',
+    f'Make your day succesfull by listening to{p_c}',
+    f'This is{p_c}',
+    f'Tune into{p_c}',
+    f'Welcome to{p_c}'
+]
+
+TEACHINGS = [
+    'our Rūpānuga Guru-varga',
+    'our Vaiṣṇavas Ācāryas',
+    'the Śrī Gauḍīya Vaiṣṇavas'
+]
+
+JOIN = [
+    'Delve deeper into',
+    'Explore and connect with',
+    'Learn, grow, and connect personally on'
+]
+
+OUTRO = [
+    'So, let\'s dive into this valuable study together and learn about this wanderful process',
+    'So, sit back, relax, and listen attentively as we embark on this spiritual journey together',
+    'Without further ado, sit back, relax and, listen attentively',
+    'Without further ado, sit back, relax, and simply "lend us your ears"',
+    'You\'ve been eagerly waiting for this, and so have we. Sit back, relax and, listen attentively'
+]
 
 class Private(commands.Cog):
     """─=≡Σ(つಠ益ಠ)つ private cog for my personal discord guild, it won't work on yours! """
     def __init__(self, bot):
         self.bot = bot
-        self.mods = [
-            ('null', 323578534763298816), ('DKD', 358420835108913153), ('herr', 439734567197474816),
-            ('TDd', 468747865351520257), ('alt', 324040201225633794), ('bvvm', 358433182304960513)
-        ]
+        self.desc = 'Command for my personal Guild'
+        self.poke = '<@&358429415417446411>'
+
 
     # +------------------------------------------------------------+
     # |                 PUSH-NOTIFICATION                          |
     # +------------------------------------------------------------+
-    @commands.command(description='Personal Guild cmd', aliases=['poke', 'push', 'notification'], no_pm=True)
-    async def nudge(self, ctx, *, _event_today: str = None):
+    @commands.command(description=self.desc, aliases=['poke', 'push', 'nudge'], no_pm=True)
+    async def radhe(self, ctx, *, _event_today: str = None):
         """
         ─=≡Σ(つಠ益ಠ)つ command to send a Push-notification in text-channel
         
         **Usage:**
-        {prefix}poke
-        {prefix}nudge [event or celebration]
-        {prefix}nudge extra [recorded podcast]
+        {prefix}radhe
+        {prefix}poke extra [write the event do you want to announce here]
         """
         try:
             await ctx.message.delete()
         except discord.Forbidden:
             pass
 
-        if ctx.author.id not in (mod[1] for mod in self.mods):
-            return
-
         channel =  ctx.channel or ctx.message.channel
         err_m = f"{ctx.message.author.mention}, update this channel's **Topic**.\n\n" \
-                f"**Tip:** check other channel's Topics to get an idea of how to format it here."
+                f"**Tip:** ask a Mod for help setting up this channel for the command to work."
 
         if isinstance(channel, discord.TextChannel):
             if not channel.topic:
                 return await ctx.send(err_m, delete_after=23)
 
+            if 'MODS' in channel.topic:
+                c = channel.topic.split('MODS')[-1]
+                w = c.split('\n\n')[0]
+                _mods = "MODS{}".format(repr(w)).replace('"', '')
+                if ctx.author.id not in (mod[1] for mod in _mods):
+                    _ask_for_help= 'You cannot use this command, contact a Mod for help.'
+                    return await ctx.send(_ask_for_help, delete_after=23)
+
             if '—' in channel.topic:
-                c_topic = channel.topic.replace('\n', '— ').split('—')[-1:]
-
-                # FIRST | Date and times
-                i = f":flag_in: | {str(t.now(z('Asia/Calcutta')).strftime(f'%A %b %d, **%H:**%M:%S'))} `(IST)`\n" \
-                    f":flag_gb: | {str(t.now(z('Europe/London')).strftime(f'%A %b %d, **%H:**%M:%S'))} `(BST)`\n" \
-                    f":flag_us: | {str(t.now(z('America/New_York')).strftime(f'%A %b %d, **%H:**%M:%S'))} `(EST)`\n" \
-                    f":flag_us: | {str(t.now(z('America/Los_Angeles')).strftime(f'%A %b %d, **%H:**%M:%S'))} `(PST)`\n" \
-                    f":flag_bo: | {str(t.now(z('America/La_Paz')).strftime(f'%A %b %d, **%H:**%M:%S'))} `(BOT)`\n\n"
-
-                date_fix = i.replace('11,', '11ᵗʰ,').replace('12,', '12ᵗʰ,').replace('13,', '13ᵗʰ,')\
-                            .replace('1,', '1ˢᵗ,').replace('2,', '2ⁿᵈ,').replace('3,', '3ʳᵈ,').replace('4,', '4ᵗʰ,')\
-                            .replace('5,', '5ᵗʰ,').replace('6,', '6ᵗʰ,').replace('7,', '7ᵗʰ,').replace('8,', '8ᵗʰ,')\
-                            .replace('9,', '9ᵗʰ,').replace('0,', '0ᵗʰ,')
-
-                # SECOND | Currently listening
-                _welcome = random.choice([
-                            f"{date_fix}\U0001f538 Welcome to the **{ctx.message.guild}** podcast",
-                            f"{date_fix}\U0001f538 Hello and welcome to the **{ctx.message.guild}** podcast",
-                            f"{date_fix}\U0001f538 This is the **{ctx.message.guild}** podcast",
-                            f"{date_fix}\U0001f538 You are currently listening to the **{ctx.message.guild}** podcast",
-                            f"{date_fix}\U0001f538 It is a beautiful day to listen to the **{ctx.message.guild}** podcast",
-                            f"{date_fix}\U0001f538 You are listening to the **{ctx.message.guild}** podcast",
-                            f"{date_fix}\U0001f538 Moving on you are listening to the **{ctx.message.guild}** podcast",
-                            f"{date_fix}\U0001f538 It is a nice day to listen to the **{ctx.message.guild}** podcast",
-                            f"{date_fix}\U0001f538 It's a perfect day to listen to the **{ctx.message.guild}** podcast"
-                    ])
-
-                # THIRD | if current Host cannot do the podcast
-                if _event_today is not None and _event_today.startswith('extra'):
-                    _what = _event_today.split(' ')[1:]
-                    _together = f" where we come together for the purpose of listening to: **{' '.join(_what)}**"
-                    _p = channel.topic.split('—')[-1]
-                    _nudge_ping = _p.split()[0]
-
-                    m = f"\u200b{_welcome}{_together}\n\n{_nudge_ping} join the Voice Channel in this Room."
-
-                else:
-                    # FOURTH | Alternate special event
-                    if _event_today is not None:
-                        h = random.choice(['for the celebration of', 'to observe', 'to honour'])
-                        v = f' where we gather together {h} **{_event_today}**.'
-
-                    # FIFTH | continuation of previous podcast
-                    else:
-                        v = random.choice([
-                            " where we shall continue with our favourite topic of discussion.",
-                            " where we'll pick up from yesterday's topic.",
-                            " where we'll continue from where we left off on the last broadcast.",
-                            " where we'll continue our immersion into our favourite topic.",
-                            ", let's take part in the continuation of our ongoing discussion."
-                        ])
-
-                    m = f"\u200b{_welcome}{v}\n\n{''.join(c_topic)}"
-
-                # SIXTH | completed message
-                _nudge = await ctx.send(m)
-
-                try:
-                    await asyncio.sleep(3)
-                    await _nudge.add_reaction('thankful:695101751707303998')
-                except discord.HTTPException:
-                    pass
-
+                _host = channel.topic.split('—')[-1]
             else:
-                await ctx.send(err_m, delete_after=23)
+                _check = 'You cannot use this command, contact a Mod for help.'
+                    return await ctx.send(_check, delete_after=23)
 
-    # +------------------------------------------------------------+
-    # |              WEB ARTICLE TO HASTEBIN                       |
-    # +------------------------------------------------------------+
-    '''
-    @commands.command(description='Personal Guild cmd', no_pm=True)
-    async def bb(self, ctx, *, _page: str = None):
-        """
-        ─=≡Σ(つಠ益ಠ)つ command to retrieve website and upload text
-        
-        **Usage:**
-        {prefix}bb [article-name]
-        """
-        if _page is None:
-            return await ctx.send('write article name as it appears in search result', delete_after=23)
-        
-        _HEADERS = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR '
-                               '2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; MS-RTC LM 8; '
-                               'InfoPath.3; .NET4.0C; .NET4.0E) chromeframe/8.0.552.224',
-                               'Accept-Language': 'en-us'}  # define headers to emulate a web browser
-        query_url = 'https://bhaktabandhav.org/'
-        base = 'https://hasteb.in'
-        url = f"{query_url}{_page.replace(' ', '-')}"
-        _web_page = requests.get(url, headers=_HEADERS)
-        _section_content = SoupStrainer('div', class_ = 'entry-content')
-        x = BeautifulSoup(_web_page.content, 'html.parser', parse_only=_section_content, from_encoding='utf-8').prettify()
-        i = x.replace('</p>', '').replace('<p>', '').replace('<div class="entry-content">', url).replace('<br/>', '').replace('</div>', 'End of article.').replace('  ', '')
+
+        def get_ordinal_suffix(num):
+            if 11 <= num % 32 <= 13:
+                suffix = 'ᵗʰ'
+            else:
+                suffix = {1: 'ˢᵗ', 2: 'ⁿᵈ', 3: 'ʳᵈ'}.get(num % 10, 'ᵗʰ')
+            return suffix
+
+        def format_date(date_str):
+            pattern = r'(\d{1,2}),'
+            day = int(re.search(pattern, date_str).group(1))
+            suffix = get_ordinal_suffix(day)
+            return date_str.replace(f"{day},", f"{day}{suffix},")
+
+        def get_t_str():
+            t_str = []
+            for code, tz_name in _TZ.items():
+                tz = z(tz_name)
+                t_now = t.now(tz)
+                suffix = get_ordinal_suffix(t_now.day)
+                flag_moji = f":flag_{code.lower().replace('ist', 'in').replace('bst', 'gb').replace('est', 'us').replace('pst', 'us').replace('bot', 'bo')}:"
+                t_str.append(f"{flag_moji}「{code}」{t_now.strftime('**%H**:%M:%S, %A %b %d' + suffix + ', %Y')}")
+            return "\n".join(t_str)
+
+        # This is still here in case there is the need for a personalised Notification
+        if _event_today is not None and _event_today.startswith('extra'):
+            _what = _event_today.split(' ')[1:]
+            _notif = 'https://cdn.discordapp.com/attachments/375179500604096512/1079876674235154442/flyerdesign_27022023_172353.png'
+            em = discord.Embed(colour=discord.Colour(0xff7722), description=get_t_str())
+                        .add_field(name='Attentive Listeners', value=_what, inline=False)\
+                        .set_thumbnail(url=_notif)\
+                        .set_footer(text='⇐ Join the Voice Channel NOW!!')
+            return await ctx.send(content=self.poke, embed=em)
+
+
+        def _intro():
+            return f'\u200b{random.choice(INTRO)}where we explore the teachings of {random.choice(TEACHINGS)}. Join our host {_host} for a thought-provoking discussion. {random.choice(JOIN)} your spiritual journey throught the path of Rūpānuga Ujjvala Mādhurya-prema.\n\n{random.choice(OUTRO)}.'
+
         try:
-            async with aiohttp.ClientSession() as session:
-                async with session.post(base + '/documents', data=i) as responce:
-                    key = (await responce.json())['key']
-            await ctx.send(f'<{base}/{key}>')
-        except Exception as e:
-            await ctx.send(f'```py\n{e}\n```', delete_after=23)
-    '''
+            em = discord.Embed(colour=discord.Colour(0xff7722), description=get_t())
+                        .add_field(name='Attentive Listeners', value=_intro, inline=False)\
+                        .set_thumbnail(url='https://i.imgur.com/93A0Kdk.png')\
+                        .set_footer(text='⇐ Join the Voice Channel NOW!!')
+            await ctx.send(content=self.poke, embed=em)
+
+        except discord.Forbidden:
+            _simple = f'{self.poke}\n{get_t()}\n\n{_intro}'
+            await ctx.send(_simple)
+
+        try:
+            await asyncio.sleep(2)
+            await _nudge.add_reaction('thankful:695101751707303998')
+        except discord.HTTPException:
+            pass
 
 async def setup(bot):
     await bot.add_cog(Private(bot))
-    
-
-# @commands.has_any_role('Admin', 'Mod', 'DJ', 'Owner')
