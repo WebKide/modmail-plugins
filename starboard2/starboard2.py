187ff23e1a25a27cbb883ffcd2c841f3cb720f60
kuro <46526001+WebKide@users.noreply.github.com>
2023-03-01 08:30:28 +0000

    channel_id

--- starboard2/starboard2.py
+++ starboard2/starboard2.py
@@ -8,7 +8,7 @@ class Starboard(commands.Cog):
         self.bot = bot
         self.star_emoji = '⭐'
         self.star_count = 3
-        self.starboard_channel_id = 123456789 # Replace with the ID of your Starboard channel
+        self.starboard_channel_id = 729093473487028254 # ID of my Starboard channel
     
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
         if payload.emoji.name != self.star_emoji:
