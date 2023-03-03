a045ec0e79c61d5c92d9cf52dc8873c2e18bc4a6
kuro <46526001+WebKide@users.noreply.github.com>
2023-03-03 12:37:12 +0000

    fix?

--- starboard2/starboard2.py
+++ starboard2/starboard2.py
@@ -1,3 +1,23 @@
+"""
+MIT License
+Copyright (c) 2023 WebKide [d.id @323578534763298816]
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import discord
 
 from discord.ext import commands
@@ -10,14 +30,20 @@ class Starboard(commands.Cog):
         self.star_count = 1
         self.guild_id = 328341202103435264 # ID of my Guild
         self.starboard_channel_id = 729093473487028254 # ID of my Starboard channel
-    
+
 
     # +------------------------------------------------------------+
     # |              ADD A MESSAGE TO STARBOARD                    |
     # +------------------------------------------------------------+
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        if payload.emoji.name != '⭐': # replace "⭐" with your star emoji name
+            return
+
         if payload.channel_id == self.starboard_channel_id:
+            return
+
+        if self.starboard_channel_id is None:
             return
 
         if payload.channel_id == self.starboard_channel_id and payload.emoji.name == self.star_emoji:
@@ -52,37 +78,35 @@ class Starboard(commands.Cog):
         if payload.user_id == self.bot.user.id:
             return
 
-        channel = self.bot.get_channel(payload.channel_id)
         if not isinstance(channel, discord.TextChannel):
             return
-        
-        message = await channel.fetch_message(payload.message_id)
-        if not message:
+
+        if not message.reactions:
             return
-        
+
         starboard_channel = self.bot.get_channel(self.starboard_channel_id)
         if not isinstance(starboard_channel, discord.TextChannel):
             return
-        
-        reactions = message.reactions
+
         star_reaction = None
-        for reaction in reactions:
+        for reaction in message.reactions:
             if reaction.emoji == self.star_emoji:
                 star_reaction = reaction
                 break
-        
-        if star_reaction.count < self.star_count:
+
+        if star_reaction is None or star_reaction.count < self.star_count:
             return
-        
+
         embed = discord.Embed(description=message.content)
         embed.set_author(name=message.author.display_name, icon_url=message.author.avatar.url)
         embed.add_field(name='Jump', value=f'[to the original!]({message.jump_url})')
         embed.add_field(name='Stars', value=star_reaction.count)
+        embed.set_footer(text=f"Starboard ID: {message.id}")
         if message.attachments:
             embed.set_image(url=message.attachments[0].url)
-        
+
         await starboard_channel.send(embed=embed)
-        
+
     # +------------------------------------------------------------+
     # |              DELETE STARBOARD MESSAGE                      |
     # +------------------------------------------------------------+
@@ -114,32 +138,32 @@ class Starboard(commands.Cog):
 
         if not isinstance(starboard_channel, discord.TextChannel):
             return
-        
+
         try:
             starboard_message = starboard_channel.fetch_message(payload.message_id)
         except discord.NotFound:
             return
-        
+
         original_message_id = starboard_message.embeds[0].fields[0].value.split('/')[-1]
         channel = self.bot.get_channel(payload.channel_id)
         if not isinstance(channel, discord.TextChannel):
             return
-        
+
         message = await channel.fetch_message(original_message_id)
         if not message:
             return
-        
+
         reactions = message.reactions
         star_reaction = None
         for reaction in reactions:
             if reaction.emoji == self.star_emoji:
                 star_reaction = reaction
                 break
-        
+
         if not star_reaction:
             await starboard_message.delete()
             return
-        
+
         if star_reaction.count < self.star_count:
             await starboard_message.delete()
             return
