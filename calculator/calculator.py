a170cd4ddd8a7857930a0c8ba0113eb85b79d70c
kuro <46526001+WebKide@users.noreply.github.com>
2023-03-07 16:13:29 +0000

    starting to migrate library

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -17,126 +17,80 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-import discord, math, operator
+import discord, sympy
 
 from discord.ext import commands
-from pyparsing import (
-    Literal,
-    CaselessLiteral,
-    Word,
-    Combine,
-    Group,
-    Optional,
-    ZeroOrMore,
-    Forward,
-    nums,
-    alphas,
-    oneOf,
-)
-
-__author__='Paul McGuire'
-__version__ = '$Revision: 0.0 $'
-__date__ = '$Date: 2009-03-20 $'
-__source__ = """http://pyparsing.wikispaces.com/file/view/fourFn.py
-http://pyparsing.wikispaces.com/message/view/home/15549426
-"""
-__note__ = """
-This is a re-wrap of Paul McGuire's fourFn.py as a class, so it can 
-be used easily in modmail-plugin. Most of the discord.py work was done 
-by corpnewt, I simply cleaned the code and added the results into embeds.
-Also, error messages delete after some time, except if answer is correct.
-"""
 
 
-class NumericStringParserForPython3(object):
-    """
-    Most of this code comes from the fourFn.py pyparsing example
-
-    """
-    def pushFirst(self, strg, loc, toks):
-        self.exprStack.append(toks[0])
-    def pushUMinus(self, strg, loc, toks):
-        if toks and toks[0]=='-':
-            self.exprStack.append('unary -')
+class Calculator(commands.Cog)::
     def __init__(self):
-        """
-        Please use any of the following symbols:
-        expop   :: '^'
-        multop  :: '*' | '/'
-        addop   :: '+' | '-'
-        integer :: ['+' | '-'] '0'..'9'+
-        """
-        point = Literal(".")
-        e     = CaselessLiteral("E")
-        fnumber = Combine(Word("+-"+nums, nums) +
-                        Optional(point + Optional(Word(nums))) +
-                        Optional(e + Word("+-"+nums, nums)))
-        ident = Word(alphas, alphas+nums+"_$")
-        plus  = Literal("+")
-        minus = Literal("-")
-        mult  = Literal("*")
-        div   = Literal("/")
-        lpar  = Literal("(").suppress()
-        rpar  = Literal(")").suppress()
-        addop  = plus | minus
-        multop = mult | div
-        expop = Literal("^")
-        pi    = CaselessLiteral("PI")
-        expr = Forward()
-        atom = ((Optional(oneOf("- +")) +
-                (pi|e|fnumber|ident+lpar+expr+rpar).setParseAction(self.pushFirst))
-                | Optional(oneOf("- +")) + Group(lpar+expr+rpar)
-               ).setParseAction(self.pushUMinus)
-        # by defining exponentiation as "atom [ ^ factor ]..." instead of
-        # "atom [ ^ atom ]...", we get right-to-left exponents, instead of left-to-right
-        # that is, 2^3^2 = 2^(3^2), not (2^3)^2.
-        factor = Forward()
-        factor << atom + ZeroOrMore((expop + factor).setParseAction(self.pushFirst))
-        term = factor + ZeroOrMore((multop + factor).setParseAction(self.pushFirst))
-        expr << term + ZeroOrMore((addop + term).setParseAction(self.pushFirst))
-        # addop_term = (addop + term).setParseAction(self.pushFirst)
-        # general_term = term + ZeroOrMore(addop_term) | OneOrMore(addop_term)
-        # expr <<  general_term
-        self.bnf = expr
-        # this will map operator symbols to their corresponding arithmetic operations
-        epsilon = 1e-12
-        self.opn = {
-                "+" : operator.add,
-                "-" : operator.sub,
-                "*" : operator.mul,
-                "/" : operator.truediv,
-                "^" : operator.pow }
-        self.fn  = {
-                "sin" : math.sin,
-                "cos" : math.cos,
-                "tan" : math.tan,
-                "abs" : abs,
-                "trunc" : lambda a: int(a),
-                "round" : round,
-                "sgn" : lambda a: abs(a)>epsilon and cmp(a,0) or 0}
+        self.exprStack = []
+        self.ops = {
+            "+" : sympy.Add,
+            "-" : sympy.Subtract,
+            "*" : sympy.Multiply,
+            "/" : sympy.Divide,
+            "^" : sympy.Pow,
+            "sin": sympy.sin,
+            "cos": sympy.cos,
+            "tan": sympy.tan,
+            "exp": sympy.exp,
+            "log": sympy.log,
+            "ln": sympy.ln
+        }
+
     def evaluateStack(self, s):
         op = s.pop()
         if op == 'unary -':
             return -self.evaluateStack(s)
-        if op in "+-*/^":
-            op2 = self.evaluateStack(s)
-            op1 = self.evaluateStack(s)
-            return self.opn[op](op1, op2)
-        elif op == "PI":
-            return math.pi # 3.1415926535
-        elif op == "E":
-            return math.e  # 2.718281828
-        elif op in self.fn:
-            return self.fn[op](self.evaluateStack(s))
-        elif op[0].isalpha():
-            return 0
+        elif isinstance(op, sympy.Basic):
+            return op
+        elif op in self.ops:
+            if op in ["sin", "cos", "tan", "exp", "ln"]:
+                return self.ops[op](self.evaluateStack(s))
+            elif op == "log":
+                arg2 = self.evaluateStack(s)
+                arg1 = self.evaluateStack(s)
+                return self.ops[op](arg1, arg2)
+            else:
+                arg2 = self.evaluateStack(s)
+                arg1 = self.evaluateStack(s)
+                return self.ops[op](arg1, arg2)
         else:
-            return float(op)
-    def eval(self,num_string,parseAll=True):
+            return sympy.S(op)
+
+    def parse(self, num_string):
         self.exprStack=[]
-        results=self.bnf.parseString(num_string,parseAll)
-        val=self.evaluateStack(self.exprStack[:])
-        return val
+        results = []
+        for tok in num_string.split():
+            if tok in self.ops:
+                results.append(tok)
+            elif tok == "pi":
+                results.append(sympy.pi)
+            elif tok == "e":
+                results.append(sympy.E)
+            else:
+                results.append(tok)
+        return results
+
+    def eval(self, num_string):
+        expr = self.parse(num_string)
+        for tok in expr:
+            if isinstance(tok, sympy.Basic):
+                self.exprStack.append(tok)
+            elif tok == 'unary -':
+                self.exprStack.append(tok)
+            elif tok in self.ops:
+                numargs = self.ops[tok].nargs
+                if numargs == 1:
+                    self.exprStack.append(tok)
+                elif numargs == 2:
+                    self.exprStack.append(tok)
+        return self.evaluateStack(self.exprStack[:])
+
+    
+    
+    
 
 class Calculator(commands.Cog):
     """(∩｀-´)⊃━☆ﾟ.*･｡ﾟ powerful calculator commands """
@@ -208,7 +162,7 @@ class Calculator(commands.Cog):
             await ctx.send(embed=e)
         except discord.Forbidden:  # FORBIDDEN (status code: 403): Missing Permissions
             await ctx.send(f'```rust\n>Input: {formula}\nResult: {success}```')
-
+'''
 
 async def setup(bot):
     await bot.add_cog(Calculator(bot))
