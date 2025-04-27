71eb4b5dd64ea751b96549a12cfb9a21038c5d38
kuro <46526001+WebKide@users.noreply.github.com>
2025-04-27 08:11:27 +0000

    Create __init__.py

---
+++ remindmepro/__init__.py
@@ -0,0 +1,7 @@
+# modmail-plugins/RemindMePro/__init__.py
+from .remindmepro import RemindMePro
+
+async def setup(bot):
+    """Setup function for the plugin"""
+    plugin = RemindMePro(bot)
+    await bot.add_cog(plugin)
