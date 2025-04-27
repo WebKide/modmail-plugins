462817d2f37d0fed80c36fe75c52b7a2df7d6a22
kuro <46526001+WebKide@users.noreply.github.com>
2025-04-27 07:45:41 +0000

    Create __init__.py

---
+++ remindmepro/tasks/__init__.py
@@ -0,0 +1,5 @@
+# modmail-plugins/RemindMePro/tasks/__init__.py
+from .service_task import ReminderServiceTask
+from .time_service import TimeService
+
+__all__ = ['ReminderServiceTask', 'TimeService']
