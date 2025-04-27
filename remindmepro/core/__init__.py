886727fe95f3c339ad2085a0b4f2f0e3b3efd2ae
kuro <46526001+WebKide@users.noreply.github.com>
2025-04-27 12:49:02 +0000

    NewClass added

--- remindmepro/core/__init__.py
+++ remindmepro/core/__init__.py
@@ -1,2 +1,15 @@
 # modmail-plugins/RemindMePro/core/__init__.py
-# Marks this directory as a Python package
+from .time import UserFriendlyTime
+from .models import Reminder
+from .storage import ReminderStorage
+from .utilities import parse_user_time, validate_future_time
+from .user_settings import UserSettings
+
+__all__ = [
+    'UserFriendlyTime',
+    'Reminder',
+    'ReminderStorage',
+    'parse_user_time',
+    'validate_future_time',
+    'UserSettings'
+]
