c6442df9b066b9ecc09250c4315994faadf1d5ff
kuro <46526001+WebKide@users.noreply.github.com>
2025-04-27 07:14:43 +0000

    Create __init__.py

---
+++ remindmepro/handlers/__init__.py
@@ -0,0 +1,7 @@
+# modmail-plugins/RemindMePro/handlers/__init__.py
+# Marks this directory as a Python package
+from .user_commands import UserCommands
+from .admin_commands import AdminCommands
+from .timezone_commands import TimezoneCommands
+
+__all__ = ['UserCommands', 'AdminCommands', 'TimezoneCommands']
