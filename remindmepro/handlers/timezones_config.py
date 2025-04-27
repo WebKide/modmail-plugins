73f2368eb4420cf74904416ce690628199805143
kuro <46526001+WebKide@users.noreply.github.com>
2025-04-27 13:41:19 +0000

    Create timezones_config.py

---
+++ remindmepro/handlers/timezones_config.py
@@ -0,0 +1,71 @@
+# In handlers/user_commands.py
+# https://gist.github.com/mjrulesamrat/0c1f7de951d3c508fb3a20b4b0b33a98
+TIMEZONE_ALIASES = {
+    # Country codes/names
+    'bolivia': 'America/La_Paz',
+    'argentina': 'America/Argentina/Buenos_Aires',
+    'australia': 'Australia/Sydney',
+    'brazil': 'America/Sao_Paulo',
+    'china': 'Asia/Shanghai',
+    'india': 'Asia/Kolkata',
+    'ireland': 'Europe/Dublin',
+    'israel': 'Asia/Jerusalem',
+    'italy': 'Europe/Rome',
+    'japan': 'Asia/Tokyo',
+    'mexico': 'America/Mexico_City',
+    'nepal': 'Asia/Kathmandu',
+    'new zealand': 'Pacific/Auckland',
+    'panama': 'America/Panama',
+    'peru': 'America/Lima',
+    'philippines': 'Asia/Manila',
+    'russia': 'Europe/Moscow',
+    'south africa': 'Africa/Johannesburg',
+    'spain': 'Europe/Madrid',
+    'uk': 'Europe/London',
+    'us': 'America/New_York',
+    
+    # Timezone abbreviations
+    'BOT': 'America/La_Paz',
+    'ART': 'America/Argentina/Buenos_Aires',
+    'EST': 'America/New_York',
+    'PST': 'America/Los_Angeles',
+    'MST': 'America/Denver',
+    'CST': 'America/Chicago',
+    'GMT': 'Europe/London',
+    'IST': 'Asia/Kolkata',
+    'JST': 'Asia/Tokyo',
+    
+    # Emoji flags
+    '🇦🇷': 'America/Argentina/Buenos_Aires',
+    '🇦🇺': 'Australia/Sydney',
+    '🇧🇷': 'America/Sao_Paulo',
+    '🇨🇳': 'Asia/Shanghai',
+    '🇩🇪': 'Europe/Berlin',
+    '🇪🇸': 'Europe/Madrid',
+    '🇫🇷': 'Europe/Paris',
+    '🇬🇧': 'Europe/London',
+    '🇮🇳': 'Asia/Kolkata',
+    '🇮🇹': 'Europe/Rome',
+    '🇯🇵': 'Asia/Tokyo',
+    '🇲🇽': 'America/Mexico_City',
+    '🇳🇿': 'Pacific/Auckland',
+    '🇵🇭': 'Asia/Manila',
+    '🇷🇺': 'Europe/Moscow',
+    '🇿🇦': 'Africa/Johannesburg',
+    '🇧🇴': 'America/La_Paz',
+    '🇨🇱': 'America/Santiago',
+    '🇨🇴': 'America/Bogota',
+    '🇪🇨': 'America/Guayaquil',
+    '🇬🇹': 'America/Guatemala',
+    '🇵🇪':'America/Lima',
+    '🇨🇷': 'America/Costa_Rica',
+    
+    # Phone codes (optional)
+    '+591': 'America/La_Paz',  # Bolivia
+    '+506': 'America/Costa_Rica',
+    '+1': 'America/New_York',  # US/Canada
+    '+44': 'Europe/London',    # UK
+    '+91': 'Asia/Kolkata',     # India
+    '+81': 'Asia/Tokyo',       # Japan
+    '+52': 'America/Mexico_City',  #Mexico
+}
