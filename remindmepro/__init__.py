58bfc2078fa90d5b68cec572319932289f86e00f
kuro <46526001+WebKide@users.noreply.github.com>
2025-04-29 03:54:11 +0000

    Update __init__.py

--- remindmepro/__init__.py
+++ remindmepro/__init__.py
@@ -3,5 +3,11 @@ from .remindmepro import RemindMePro
 
 async def setup(bot):
     """Setup function for the plugin"""
+    await bot.add_cog(RemindMePro(bot))
+
+'''
+async def setup(bot):
+    """Setup function for the plugin"""
     plugin = RemindMePro(bot)
     await bot.add_cog(plugin)
+'''
