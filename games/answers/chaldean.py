34056822866752956ffce11a103b83161c066165
kuro <46526001+WebKide@users.noreply.github.com>
2025-05-03 03:09:03 +0000

    Created chaldean.py

---
+++ games/answers/chaldean.py
@@ -0,0 +1,156 @@
+chaldean_list = [
+    # 01 Sun
+    {
+        'chaldean_number': '1',  # number from 1 to 99, based on Linda Goodman’s Sun Signs
+        'chaldean_planet': 'Represented by the Celestial body of the Sun',  # planet that the number represents
+        'chaldean_keywords': 'Leadership, independence, creation',   #3 keywords for the number
+        'chaldean_title': '',  # what is the number called
+        'chaldean_day': '',  # meaning if one is born on this day in the month (1 to 31)
+        'chaldean_dob': '',  # meaning of the composite reduction (14+12+2011=3)
+        'chaldean_vowels_soul_urge': '',  # sum of only vowels in the name or string
+        'chaldean_consonants_personality': '',  # sum of only consonants in the name or string
+        'chaldean_full_name_destiny': '',  # sum total of vowels and consonants
+        'chaldean_first_name': '',  #sum total of vowels and consonants in the given name (ie "john" doe)
+        'chaldean_tablets': 'No description',  # if there is a description of the tablets, it goes here
+        'chaldean_meaning': '',  # meaning of total name and dob as final number
+        'chaldean_interpretation': '',  # general interpretation of the number
+        'chaldean_colour': '0xff00ff'  # colour linked to the planet the number represents
+    },
+    # 02 Moon
+    {
+        'chaldean_number': '2', 
+        'chaldean_planet': 'Represented by the Celestial body of the Moon', 
+        'chaldean_keywords': 'Harmony, cooperation, sensitivity', 
+        'chaldean_title': '', 
+        'chaldean_day': '', 
+        'chaldean_dob': '', 
+        'chaldean_vowels_soul_urge': '', 
+        'chaldean_consonants_personality': '', 
+        'chaldean_full_name_destiny': '', 
+        'chaldean_first_name': '', 
+        'chaldean_tablets': 'No description', 
+        'chaldean_meaning': '', 
+        'chaldean_interpretation': '',
+        'chaldean_colour': '0xff00ff'
+    },
+    # 03 Jupiter
+    {
+        'chaldean_number': '3', 
+        'chaldean_planet': 'Represented by the Celestial body of Jupiter', 
+        'chaldean_keywords': 'Creativity, expression, communication', 
+        'chaldean_title': '', 
+        'chaldean_day': '', 
+        'chaldean_dob': '', 
+        'chaldean_vowels_soul_urge': '', 
+        'chaldean_consonants_personality': '', 
+        'chaldean_full_name_destiny': '', 
+        'chaldean_first_name': '', 
+        'chaldean_tablets': 'No description', 
+        'chaldean_meaning': '', 
+        'chaldean_interpretation': '',
+        'chaldean_colour': '0xff00ff'
+    },
+    # 04 Uranus
+    {
+        'chaldean_number': '4', 
+        'chaldean_planet': 'Represented by the Celestial body of Uranus', 
+        'chaldean_keywords': 'Stability, organization, hard work', 
+        'chaldean_title': '', 
+        'chaldean_day': '', 
+        'chaldean_dob': '', 
+        'chaldean_vowels_soul_urge': '', 
+        'chaldean_consonants_personality': '', 
+        'chaldean_full_name_destiny': '', 
+        'chaldean_first_name': '', 
+        'chaldean_tablets': 'No description', 
+        'chaldean_meaning': '', 
+        'chaldean_interpretation': '',
+        'chaldean_colour': '0xff00ff'
+    },
+    # 05 Mercury
+    {
+        'chaldean_number': '5', 
+        'chaldean_planet': 'Represented by the Celestial body of Mercury', 
+        'chaldean_keywords': 'Freedom, adventure, change', 
+        'chaldean_title': '', 
+        'chaldean_day': '', 
+        'chaldean_dob': '', 
+        'chaldean_vowels_soul_urge': '', 
+        'chaldean_consonants_personality': '', 
+        'chaldean_full_name_destiny': '', 
+        'chaldean_first_name': '', 
+        'chaldean_tablets': 'No description', 
+        'chaldean_meaning': '', 
+        'chaldean_interpretation': '',
+        'chaldean_colour': '0xff00ff'
+    },
+    # 06 Venus
+    {
+        'chaldean_number': '6', 
+        'chaldean_planet': 'Represented by the Celestial body of Venus', 
+        'chaldean_keywords': 'Responsibility, love, family', 
+        'chaldean_title': '', 
+        'chaldean_day': '', 
+        'chaldean_dob': '', 
+        'chaldean_vowels_soul_urge': '', 
+        'chaldean_consonants_personality': '', 
+        'chaldean_full_name_destiny': '', 
+        'chaldean_first_name': '', 
+        'chaldean_tablets': 'No description', 
+        'chaldean_meaning': '', 
+        'chaldean_interpretation': '',
+        'chaldean_colour': '0xff00ff'
+    },
+    # 07 Neptune
+    {
+        'chaldean_number': '7', 
+        'chaldean_planet': 'Represented by the Celestial body of Neptune', 
+        'chaldean_keywords': 'Wisdom, spirituality, introspection', 
+        'chaldean_title': '', 
+        'chaldean_day': '', 
+        'chaldean_dob': '', 
+        'chaldean_vowels_soul_urge': '', 
+        'chaldean_consonants_personality': '', 
+        'chaldean_full_name_destiny': '', 
+        'chaldean_first_name': '', 
+        'chaldean_tablets': 'No description', 
+        'chaldean_meaning': '', 
+        'chaldean_interpretation': '',
+        'chaldean_colour': '0xff00ff'
+    },
+    # 08 Saturn
+    {
+        'chaldean_number': '8', 
+        'chaldean_planet': 'Represented by the Celestial body of Saturn', 
+        'chaldean_keywords': 'Power, abundance, ambition', 
+        'chaldean_title': '', 
+        'chaldean_day': '', 
+        'chaldean_dob': '', 
+        'chaldean_vowels_soul_urge': '', 
+        'chaldean_consonants_personality': '', 
+        'chaldean_full_name_destiny': '', 
+        'chaldean_first_name': '', 
+        'chaldean_tablets': 'No description', 
+        'chaldean_meaning': '', 
+        'chaldean_interpretation': '',
+        'chaldean_colour': '0xff00ff'
+    },
+    # 09 Mars
+    {
+        'chaldean_number': '9', 
+        'chaldean_planet': 'Represented by the Celestial body of Mars', 
+        'chaldean_keywords': 'Compassion, humanity, altruism', 
+        'chaldean_title': '', 
+        'chaldean_day': '', 
+        'chaldean_dob': '', 
+        'chaldean_vowels_soul_urge': '', 
+        'chaldean_consonants_personality': '', 
+        'chaldean_full_name_destiny': '', 
+        'chaldean_first_name': '', 
+        'chaldean_tablets': 'No description', 
+        'chaldean_meaning': '', 
+        'chaldean_interpretation': '',
+        'chaldean_colour': '0xff00ff'
+    },
+
+]
