e5117a10d55b5ea4564d3a500b2c65cb4ad7f3bb
kuro <46526001+WebKide@users.noreply.github.com>
2025-05-06 03:44:25 +0000

    charinfo help

--- transform/transform.py
+++ transform/transform.py
@@ -663,10 +663,10 @@ class Transform(commands.Cog):
         """Transform 𝐔𝐧𝐢𝐜𝐨𝐝𝐞 <--> 𝐂𝐡𝐚𝐫𝐚𝐜𝐭𝐞𝐫
         
         - Show info about unicode characters:
-          - Character `@` to `\\U​0040`
+          - Character `@` to `\\U0040`
         - Convert unicode escapes to character:
-          - `\\U​0040` to character `@`
-          - `\\N​{​WHITE HEAVY CHECK MARK​}` to `✅`
+          - `\\U0040` to character `@`
+          - `\\N{{WHITE HEAVY CHECK MARK}}` to `✅`
         """
         if not characters:
             return await ctx.send_help(self.charinfo)
