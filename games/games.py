d61b929a7e57782593d2ed8d4f71b4f81be76b7e
kuro <46526001+WebKide@users.noreply.github.com>
2025-05-10 23:38:21 +0000

    0.2.12 — ```ml\n{given_choices}```

--- games/games.py
+++ games/games.py
@@ -25,7 +25,7 @@ from .answers.tarot import card_deck
 from .answers.words import scramble_words
 
 logger = logging.getLogger("Modmail")
-__version__ = "0.2.11 — random.choice(choices).title()"
+__version__ = "0.2.12 — ```ml\n{given_choices}```"
 
 
 class GamesTracker:
@@ -382,7 +382,7 @@ class Games(commands.Cog):
         embed = await self.embed_manager.create_command_embed(
             ctx,
             title=f'🙈 Options given by {ctx.author.display_name}:',
-            description=f'{given_choices}\n### {Emoji.DIAMOND} Selection:\n# I choose: **{picked}**',
+            description=f'```ml\n{given_choices}```\n### {Emoji.DIAMOND} I choose:\n# {picked}',
             start_time=start_time
         )
         await ctx.send(embed=embed)
