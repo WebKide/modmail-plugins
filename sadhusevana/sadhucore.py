86007b5f8118e74920b48ad55e3a309ac2ba032e
kuro <46526001+WebKide@users.noreply.github.com>
2026-05-10 02:14:16 +0000

    Update sadhucore.py [4]

--- sadhusevana/sadhucore.py
+++ sadhusevana/sadhucore.py
@@ -53,7 +53,7 @@ class SadhuUI:
                 suffix = SadhuUI.get_ordinal_suffix(t_now.day)
 
                 flag_emoji = SadhuUI.EMOJI_MAP.get(code, f":flag_{code.lower()}:")
-                date_str = t_now.strftime('**%H**:%M:%S — %A, %b %d, %Y')
+                date_str = t_now.strftime('**%H**:%M | %A, %b %d')  # date_str = t_now.strftime('**%H**:%M:%S — %A, %b %d, %Y')
                 date_str = date_str.replace(f"{t_now.day},", f"{t_now.day}{suffix},")
 
                 city_name = tz_path.split("/")[-1].replace("Kolkata", "Māyāpura").replace("La_Paz", "Cochabamba").replace("_", " ")  # NEW VALUE
